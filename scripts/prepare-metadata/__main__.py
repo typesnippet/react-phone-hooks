907b001dccaf2984b17bf977deaff6adb5856c04
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2025-12-01 18:48:51 +0400

    Fix the mask generator script

--- scripts/prepare-metadata/__main__.py
+++ scripts/prepare-metadata/__main__.py
@@ -13,21 +13,20 @@ territories = tree.find("territories")
 
 
 def update_mask(mask, length):
-    length_without_cc = len(re.findall(r"[\d.]", re.sub(r"\+\d+\s", "", mask)))
+    length_without_cc = re.sub(r"^[^)]+\)\s", "", mask).count(".")
     if length_without_cc != length:
-        cc_mask = re.match(r"(\+\d+)\s", mask).group(1)
-        ac_mask = re.match(r"\+\d+\s(?:(\S+)\s)?", mask).group(1) or ""
-        ac_mask_length = len(re.findall(r"[\d.]", ac_mask))
-        pn_mask_expected_length = length - ac_mask_length
-        pn_mask_prefix = re.sub(r"\+\d+\s(?:\S+\s)?", "", mask).split()[0]
+        cc_mask = re.match(r"(\+[^(]+)\s", mask).group(1)
+        ac_mask = re.match(r"\+[^(]+(?:(\S+)\s)?", mask).group(1) or ""
+        pn_mask_prefix = re.sub(r"\+[^(]+(?:\S+\s)?", "", mask).split()[0]
+        pn_mask_expected_length = length - len(re.findall(r"[\d.]", ac_mask)) - len(re.findall(r"\d", pn_mask_prefix))
         pn_mask_parts = [pn_mask_prefix]
         while len("".join(pn_mask_parts)) < pn_mask_expected_length:
             pn_mask_parts.append("." * len(pn_mask_prefix))
         pn_mask = " ".join(pn_mask_parts)
-        while len(pn_mask.replace(" ", "")) > pn_mask_expected_length:
+        while pn_mask.count(".") > pn_mask_expected_length:
             pn_mask = pn_mask[:-1]
         mask = f"{cc_mask} {ac_mask} {pn_mask}" if ac_mask else f"{cc_mask} {pn_mask}"
-    return re.sub(r"\s(\.)$", r"\1", mask)
+    return re.sub(r"\s(\.{1,2})$", r"\1", mask)
 
 
 with open(patterns_path) as fp:
