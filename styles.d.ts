effac884e84d04eb4d86c051c256eae0e020f3a6
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2025-12-11 16:41:04 +0400

    Delete build artifacts

--- styles.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const jsonToCss: (stylesheet: object) => string;
-export declare const injectStyles: (cssText: string) => void;
--- types.cjs.js
+++
@@ -1,3 +0,0 @@
-"use client";
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
